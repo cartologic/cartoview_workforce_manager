b2de2a2c2f220142cd9ba938fc3cb8869b7b967c
Amira-Mohiey <amira.mohiey@cartologic.com>
2017-09-13 12:58:23 +0200

    fix add task bug

--- admin.py
+++ admin.py
@@ -5,8 +5,7 @@ from simple_history.admin import SimpleHistoryAdmin
 from django.contrib import admin
 from .models import Task,Project,ProjectDispatchers,ProjectWorkers,Attachment
 
-admin.site.register(Task,SimpleHistoryAdmin)
-admin.site.register(Project,SimpleHistoryAdmin)
+
 admin.site.register(ProjectDispatchers)
 admin.site.register(ProjectWorkers)
 admin.site.register(Attachment)
