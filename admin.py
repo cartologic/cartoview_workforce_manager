ad2f957545ec21c1caa9751bfe05f24291931317
Ahmed Nour Eldeen <ahmednoreldeen@gmail.com>
2017-09-13 14:46:48 +0200

    remove unused simple_history package

--- admin.py
+++ admin.py
@@ -1,7 +1,6 @@
 
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
-from simple_history.admin import SimpleHistoryAdmin
 from django.contrib import admin
 from .models import Task,Project,ProjectDispatchers,ProjectWorkers,Attachment
 
