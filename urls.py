7c8d74ebb65e180ffbdf9679917a4684963a4457
Amira-Mohiey <amira.mohiey@cartologic.com>
2017-09-13 12:56:23 +0200

    fix add task bug

--- urls.py
+++ urls.py
@@ -18,5 +18,7 @@ urlpatterns = patterns('',url(r'^new/$', views.new, name='%s.new' % APP_NAME),
 url(r'^(?P<instance_id>\d+)/edit/$',views.edit, name='%s.edit' % APP_NAME),
 url(r'^(?P<instance_id>\d+)/view/$',views.view_app, name='%s.view' % APP_NAME),
 url(r'^api/', include(v1_api.urls)),
+url(r'^test/',TemplateView.as_view(template_name='cartoview_workforce_manager/test.html')),
+
 
 )
